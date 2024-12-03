96ad9e2b915863b4746b4ef47288d300d8dcbe58
smixs <mixshima@gmail.com>
2024-12-03 12:24:52 +0500

    Remove GitHub Pages config for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/shima.me',
-  assetPrefix: '/shima.me',
   eslint: {
     ignoreDuringBuilds: true,
   },
